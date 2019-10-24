525f9af0e7d8c2f74d223d6c2756a51339b357a7
paul <omd.paul@gmail.com>
2019-10-24 11:08:00 +0300

    register & viewurl

---
+++ app/src/main/java/com/encorsa/wandr/logInFragments/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.encorsa.wandr.logInFragments.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/encorsa/wandr/logInFragments/viewPrivacy/ViewUrlViewModel.kt
@@ -0,0 +1,7 @@
+package com.encorsa.wandr.logInFragments.viewPrivacy
+
+import androidx.lifecycle.ViewModel
+
+class ViewUrlViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
